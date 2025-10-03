7278607cc8f5e839de06f6f62afdba09075a777e
eifr <eifr0980@gmail.com>
2025-10-04 01:23:16 +0300

    refactor: clean up App component by removing unused imports and simplifying handleMotion logic

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,10 @@
-import { useState, useCallback } from "preact/hooks";
-import preactLogo from "./assets/preact.svg";
-import viteLogo from "/vite.svg";
+import { useCallback } from "preact/hooks";
 import "./app.css";
 import MotionDetector from "./components/Motion";
 import { TelegramSettings } from "./components/TelegramSettings";
 import { useTelegram } from "./hooks/useTelegram";
 
 export function App() {
-  const [count, setCount] = useState(0);
   const {
     telegramBotToken,
     setTelegramBotToken,
@@ -23,10 +20,10 @@ export function App() {
 
   const handleMotion = useCallback(
     (_: Date, frame: string) => {
-      console.log("Motion detected!");
-      if (sendTelegrams) {
-        sendTelegramMessage(frame);
+      if (!sendTelegrams) {
+        return;
       }
+      sendTelegramMessage(frame);
     },
     [sendTelegrams, sendTelegramMessage]
   );
@@ -34,6 +31,8 @@ export function App() {
   return (
     <>
       <div>
+        <h1>Vigilo</h1>
+
         <TelegramSettings
           sendTelegrams={sendTelegrams}
           setSendTelegrams={setSendTelegrams}
@@ -51,35 +50,7 @@ export function App() {
           motionPixelRatio={0.01}
           intervalMs={200}
         />
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
       </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{" "}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
     </>
   );
 }
