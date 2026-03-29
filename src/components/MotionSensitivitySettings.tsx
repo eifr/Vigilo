bbb3aca3e63b3be5e78d99d23766dbb801a1ebee
eifr <eifr0980@gmail.com>
2026-03-29 10:18:09 +0200

    remove unused var

--- src/components/MotionSensitivitySettings.tsx
+++ src/components/MotionSensitivitySettings.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { Input } from "@/components/ui/input";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 
 export function MotionSensitivitySettings({
   intervalMs,
